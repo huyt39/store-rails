be0fe188f97e6de80989eb742a447bb612f50a1d
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2025-08-01 10:44:00 +0700

    concern extract

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    include Notifications 
     has_many :subcribers, dependent: :destroy
     has_rich_text :description
     has_one_attached :featured_image
@@ -7,15 +8,5 @@ class Product < ApplicationRecord
     validates :inventory_count, numericality: {
         greater_than_or_equal_to: 0
     }
-    after_update_commit :notify_subscribers, if: :back_in_stock?
 
-    def back_in_stock?
-        inventory_count_previously_was.zero? && inventory_count > 0
-    end
-
-    def notify_subscribers
-        subscribers.each do |subscriber|
-        ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
-        end
-    end
 end
